d42730400c869b200f6d793e56234487f6d1d76c
rajcosiva <rajcohere@gmail.com>
2026-03-03 09:12:39 +0530

    Update UserController.java

--- backend/src/main/java/com/ust_proj/chriss_proj/controller/UserController.java
+++ backend/src/main/java/com/ust_proj/chriss_proj/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
